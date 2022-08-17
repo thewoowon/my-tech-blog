675703508d9eb8cd5425086b33156e58e3445670
thewoowon <thewoowon@naver.com>
2022-08-17 11:00:12 +0900

    20220817_UBTECH

--- pages/culture.tsx
+++ pages/culture.tsx
@@ -7,28 +7,29 @@ import uki from '../images/character/uki.svg';
 import ubfriends from '../images/character/ubfriends.svg';
 import ubfriendsNone from '../images/character/ubfriends_none.svg';
 import Image from 'next/image';
-
+import backGround_1 from '../images/background/ubtech_background_1.svg'
 
 export const Culture = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'Culture UBCARE TECH',
+        title: '유비케어 기술 블로그 | CULTURE',
       }}
     >
       <section className="culture section-1">
-          <Image src={ubfriends} alt="ubfriends" width={550} height={500} className="friends-box"></Image>
-          <p className="slogan">세상이 꿈꾸던 헬스케어</p>
-          <p className="ubf">유비 프렌즈와 함께 하세요!</p>
+       
+          <p className="slogan"><span>세상이 꿈꾸던</span></p>
+          <p className="ubf"><span>헬스케어를 만들어갑니다.</span></p>
+        
       </section>
       <section className="culture section-2">
-      <Image src={ubfriendsNone} alt="ubfriends" width={550} height={500} className="friends-box"></Image>
+          <p className="center-title text-position-1">CHALLENGE</p>
       </section>
-      <section className="culture section-3">
-        <Image src={ujune} alt="ujune" width={250} height={350} className="ujune-box"></Image>
+      <section className="culture  section-3">
+          <p className="center-title text-position-2">CHANGE</p>
       </section>
       <section className="culture section-4">
-        <Image src={uki} alt="uki" width={150} height={250} className="uki-box"></Image>
+          <p className="center-title text-position-3">RESPECT</p>
       </section>
     </Layout>
   );
