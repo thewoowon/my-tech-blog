47db62e6b458d912be6aab0ee7df7a4c5955d2af
thewoowon <thewoowon@naver.com>
2022-08-27 11:11:20 +0900

    20220827_UBTECH

--- components/Card.tsx
+++ components/Card.tsx
@@ -52,23 +52,7 @@ const Card = ():JSX.Element =>{
                     <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
                 </div>
                 </a>
-            </li>
-            <li>
-                <a href="" className="card">
-                <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
-                <div className="card__overlay">
-                    <div className="card__header">
-                    <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-                    <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-                    <div className="card__header-text">
-                        <h3 className="card__title">kim Cattrall</h3>
-                        <span className="card__status">3 hours ago</span>
-                    </div>          
-                    </div>
-                    <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                </div>
-                </a>
-            </li>    
+            </li> 
             </ul>
     )
 }
