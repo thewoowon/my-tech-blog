030638068256d0e3d3a5dc4c54539cc4337bce90
thewoowon <thewoowon@naver.com>
2022-12-29 12:41:10 +0900

    feat : Add greedy posting & chore(util): Adjust convention

--- context/mdxContext.tsx
+++ context/mdxContext.tsx
@@ -1,44 +1,44 @@
 import {
-    createContext,
-    useContext,
-    useState,
-    Dispatch,
-    ReactElement,
-    ReactNode,
-    SetStateAction,
+  createContext,
+  useContext,
+  useState,
+  Dispatch,
+  ReactElement,
+  ReactNode,
+  SetStateAction,
 } from 'react';
 
 type ContextProps = {
-    prerequisites: string[];
-    setPrerequisites: Dispatch<SetStateAction<string[]>>;
-    stacks: string[];
-    setStacks: Dispatch<SetStateAction<string[]>>;
+  prerequisites: string[];
+  setPrerequisites: Dispatch<SetStateAction<string[]>>;
+  stacks: string[];
+  setStacks: Dispatch<SetStateAction<string[]>>;
 };
 
 type Props = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
 const MdxComponentsContext = createContext({} as ContextProps);
 
 export function MdxComponentsProvider({ children }: Props): JSX.Element {
-    const [prerequisites, setPrerequisites] = useState < string[] > ([]);
-    const [stacks, setStacks] = useState < string[] > ([]);
+  const [prerequisites, setPrerequisites] = useState<string[]>([]);
+  const [stacks, setStacks] = useState<string[]>([]);
 
-    return (
-        <MdxComponentsContext.Provider
-            value={{
-                prerequisites,
-                setPrerequisites,
-                stacks,
-                setStacks,
-            }}
-        >
-            {children}
-        </MdxComponentsContext.Provider>
-    );
+  return (
+    <MdxComponentsContext.Provider
+      value={{
+        prerequisites,
+        setPrerequisites,
+        stacks,
+        setStacks,
+      }}
+    >
+      {children}
+    </MdxComponentsContext.Provider>
+  );
 }
 
 export function useMdxComponentsContext(): ContextProps {
-    return useContext(MdxComponentsContext);
-} 
+  return useContext(MdxComponentsContext);
+}
