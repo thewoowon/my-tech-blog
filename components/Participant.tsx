f72e390e1055d76a4f45a310838ed97aa654b7c4
thewoowon <thewoowon@naver.com>
2024-02-13 16:16:23 +0900

    feat: add 상세페이지용 슬라이더

--- components/Participant.tsx
+++ components/Participant.tsx
@@ -4,7 +4,7 @@ function Participant({ contents }: { contents: string }) {
   return (
     <div
       style={{ width: '400px', height: '500px' }}
-      className="shadow-lg rounded-xl p-10 text-black overflow-scroll"
+      className="shadow-lg rounded-xl p-10 text-black overflow-hidden bg-white text-left"
     >
       <div className="text-3xl font-medium">
         {"Won's Comment"}
---
+++ components/TikitakaSlider.tsx
@@ -0,0 +1,41 @@
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import React from 'react';
+import Image from 'next/image';
+
+const TikitakaSlider = (): JSX.Element => {
+  return (
+    <Swiper
+      id="carousel"
+      modules={[Autoplay, Navigation, Pagination, A11y]}
+      spaceBetween={20}
+      slidesPerView={'auto'}
+      centeredSlides={true}
+      navigation
+      // autoplay={{delay:6000,
+      //   disableOnInteraction:false}}
+      pagination={{ clickable: true }}
+      className="my-wide-swiper"
+      loop={true}
+      height={800}
+    >
+      <SwiperSlide className="my-wide-swiper-slide">
+        <div className="h-full bg-gray-100 flex justify-center items-center">
+          <Image
+            alt=""
+            src={'/assets/tikitaka_thumb.png'}
+            width="4396"
+            height="2476"
+          ></Image>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default TikitakaSlider;
